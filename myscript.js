1f75fbc5450533fa04df84a42718a74ae9b405b5
Shourja Mukherjee <shourja123@gmail.com>
2019-02-24 16:05:33 +0530

    Finished project made all tweaks

--- myscript.js
+++ myscript.js
@@ -1,7 +1,10 @@
+var ind_count=1;
+var qat_count=1;
 //LIKE COUNTER
 var clicks=localStorage.getItem('likes');
 if (isNaN(clicks))
     clicks=0;
+document.getElementById("clickcount").innerHTML="LIKES:"+clicks; //sets up button with current number of likes
 function clickFunction(){
     console.log(clicks)
     clicks++;
@@ -11,7 +14,6 @@ function clickFunction(){
 
 //FIXED NAV BAR
 $(document).ready(function(){
-    document.getElementById("clickcount").innerHTML="LIKES:"+clicks;
     $(".nav").css("position","fixed");
 });
 
@@ -35,9 +37,8 @@ var count=localStorage.getItem('count');
 if (isNaN(count))
     count=0;
 
-//console.log(count);
-
-
+document.getElementById("totalrows").innerHTML= "Total Rows:"+count; //merged table cells
+//function to insert row
 function myFunction(count) {
     var table = document.getElementById("myTable");
     console.log(count);
@@ -50,7 +51,7 @@ function myFunction(count) {
     cell2.innerHTML = listtemp["skill"];
     cell3.innerHTML= listtemp["proficiency"];
 }
-
+//initializes Table from stored value
 function initializeTable(){
     for(var i=1;i<=count;i++){
         console.log(i);
@@ -58,6 +59,7 @@ function initializeTable(){
     }
 }
 initializeTable();
+
 //TABLE STORAGE
 function getdata(){
     count++;
@@ -66,30 +68,45 @@ function getdata(){
     var proficiency=document.getElementById("proficiency").value;
     var list={"name":String(name),"skill":String(skill),"proficiency":String(proficiency)};
     dict[String(count)]=list;
-    /*
-    var list={
-        1:"[Bob,yoga,proficient]",
-        2:"[Charlie,yoga,okayish]",
-    }
-    */
-
-    //list[String(name)]=[skill,proficiency];
-
-    /*
-    var list = []; // create an empty array
-    list.push({
-        key:   String(name),
-        value: [skill,proficiency]
-    });
-    */
-
-    //list=JSON.stringify(list);
-    //var info=JSON.parse(list);
-    //console.log(info[1]);
     myFunction(count);
     localStorage.setItem('count',count);
     localStorage.removeItem('dict');
     localStorage.setItem('dict',JSON.stringify(dict));
-    //console.log(dict["2"]);
-    console.log(count);
+    document.getElementById("totalrows").innerHTML= "Total Rows:"+count; //merged table cells
+}
+
+//Slideshow for India
+function changepic_ind_prev() {
+	ind_count = ind_count -1;
+	if(ind_count < 1) {
+		ind_count = 7;
+	}
+	str = "img/ind_"+ind_count+".jpg";
+	document.getElementById("india").src = str;
+}
+function changepic_ind_next() {
+	ind_count = ind_count +1;
+	if(ind_count > 7) {
+		ind_count = 1;
+	}
+	str = "img/ind_"+ind_count+".jpg";
+	document.getElementById("india").src = str;
+}
+
+//Slideshow for Qatar
+function changepic_qat_prev() {
+	qat_count = qat_count -1;
+	if(qat_count < 1) {
+		qat_count = 6;
+	}
+	str = "img/qat_"+qat_count+".jpg";
+	document.getElementById("qatar").src = str;
+}
+function changepic_qat_next() {
+	qat_count = qat_count +1;
+	if(qat_count > 6) {
+		qat_count = 1;
+	}
+	str = "img/qat_"+qat_count+".jpg";
+	document.getElementById("qatar").src = str;
 }
